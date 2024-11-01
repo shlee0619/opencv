9ab7948f55edf601b7b033555e51b0f720505711
shlee0619 <shlee619@icloud.com>
2024-11-01 12:31:04 +0900

    2024.11.01. 12:31

--- 07videocv.py
+++ 07videocv.py
@@ -18,6 +18,31 @@ path = './data/Puppies-FHD.mp4'
 
 
 
+cap = cv2.VideoCapture(path)
+
+dummy, pupp = cap.read()
+print()
+print('dummy 결과 불', dummy)
+cv2.imshow('puppies dog', pupp)
+cv2.waitKey()
+
+
+# if cap.isOpened():
+#     delay = int(1000/cap.get(cv2.CAP_PROP_FPS))
+#     while True:
+#         ret, img = cap.read()
+#         if ret:
+#             cv2.imshow("Movie", img)
+#             if cv2.waitKey(delay) & 0xFF == 27:
+#                 print("ESC Key pressed")
+#                 break
+#         else:
+#             print(ret, img)
+#             break
+# else:
+#     print("File not opened")
+
+
 
 
 
