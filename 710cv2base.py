547f6e94b527c95fbc7236e156f57fb2b23b9c74
shlee0619 <shlee619@icloud.com>
2024-11-04 15:13:54 +0900

    Add new changes

---
+++ 710cv2base.py
@@ -0,0 +1,26 @@
+import time
+import warnings
+warnings.filterwarnings('ignore') 
+
+import matplotlib.pyplot as plt
+from matplotlib import rc ,  font_manager
+font_name = font_manager.FontProperties(fname='c:/windows/Fonts/malgun.ttf').get_name()
+rc('font', family=font_name)
+
+import numpy as np
+import cv2
+
+path = './data/a1.png'
+
+
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.axis('off')
+plt.show()
+
+# 블러blur는 
+plt.imshow(img)
+plt.xticks([])
+plt.yticks([])
+plt.show()
